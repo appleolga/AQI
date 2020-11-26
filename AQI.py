d4f936fe9e1f121563f950558279cd5692b876db
appleolga <appleolga@gmail.com>
2020-11-26 20:29:39 +0200

    initial

--- AQI.py
+++ AQI.py
@@ -24,7 +24,7 @@ data['color'] = ''
 # obtaining data from API
 for id in data.index:
     station_data = json.loads(requests.get(
-        'https://api.waqi.info/feed/@' + str(id) + '/?token=d298195980b6d5f4a11d06b3ebc43323facd8ed8').text)['data']
+        'https://api.waqi.info/feed/@' + str(id) + '').text)['data']
     data.loc[id, 'aqi'] = station_data['aqi']
     data.loc[id, 'Station name'] = re.match(r'(.*)(?:, K.*)', station_data['city']['name']).group(1)
     if int(station_data['aqi']) <= 50:
